22e562f9efad5e10867c59f7a181d44cda1e2198
Mykola Ts <tsybulskiyk@gmail.com>
2025-04-12 17:07:19 +0300

    added slider in Hero section

--- js/main.js
+++ js/main.js
@@ -19,3 +19,18 @@ validator
   .onSuccess(handlerSubmitBookingForm);
 
 selectors.openMobileMenuBtn.addEventListener("click", handlerOpenMobileMenu);
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
